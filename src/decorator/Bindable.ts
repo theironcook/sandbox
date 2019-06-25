e5b732031ddac31be5cf620d1fca90ec65df023e
Bart Wood <bartwood@gmail.com>
2019-06-25 08:23:39 -0600

    stuff

--- src/decorator/Bindable.ts
+++ src/decorator/Bindable.ts
@@ -3,23 +3,28 @@ import { createDecorator } from 'vue-class-component';
 
 
 type BindPropOptions = {
+  selectedModelName: string,
+  updateActionName: string,
   getFormatter?: (value: any) => string,
   setFormatter?: (value: string) => {cancelSetter?: boolean, value?: any}
 }
 
+const defaultBindPropOptions = {
+  selectedModelName: 'selectedCopy',
+  updateActionName: 'updateSelectedCopy'
+}
 
 // Make it easy to edit fields in a form.
 // Relies on the store to have the action updateSelectedCopy and a state variable named selectedCopy
-export const BindProp = function(storeName: string, {getFormatter, setFormatter}: BindPropOptions = {}){
-  return createDecorator(function (options: any, key: string){
-    
+export const BindProp = function(storeName: string, {selectedModelName, updateActionName, getFormatter, setFormatter}: BindPropOptions = defaultBindPropOptions){
+  return createDecorator(function (options: any, key: string){    
     if(!options.computed){
       options.computed = {};
     }
 
     options.computed[key] = {
       get: function(){
-        const selectedCopy = this.$store.state[storeName]['selectedCopy'];
+        const selectedCopy = this.$store.state[storeName][selectedModelName];
 
         if(selectedCopy){
           let value = selectedCopy[key];
@@ -36,16 +41,15 @@ export const BindProp = function(storeName: string, {getFormatter, setFormatter}
       },
 
       set: function(value){
-        if(this.$store.state[storeName]['selectedCopy']){
+        if(this.$store.state[storeName][selectedModelName]){
           if(setFormatter){
             const formattedValue = setFormatter(value);
             if(!formattedValue.cancelSetter){
-              console.log(`set ${key} to ${value}`);
-              this.$store.dispatch(`${storeName}/updateSelectedCopy`, {propName: key, value: formattedValue.value});
+              this.$store.dispatch(`${storeName}/${updateActionName}`, {propName: key, value: formattedValue.value});
             }
           }
           else {
-            this.$store.dispatch(`${storeName}/updateSelectedCopy`, {propName: key, value});
+            this.$store.dispatch(`${storeName}/${updateActionName}`, {propName: key, value});
           }                    
         }
       }
